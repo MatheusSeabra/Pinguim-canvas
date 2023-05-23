8f887f640e38b8d6ba7cfdb030c2ee9a9b4529c2
2jdmtec <2jdmtec@bglab.local>
2023-05-23 12:14:55 -0300

    lacucaratcha

--- javascript/script.js
+++ javascript/script.js
@@ -4,6 +4,17 @@ const ctx = canvas.getContext("2d");
 ctx.canvas.width = window.innerWidth;
 ctx.canvas.height = window.innerHeight;
 
+//cabeça
 ctx.beginPath();
-ctx.arc(960, 500, 80, 0, 2 * Math.PI);
+ctx.arc(450, 200, 100, 0, 1 * Math.PI, true);
 ctx.stroke();
+
+ctx.beginPath();
+ctx.arc(450, 112, 50, 0, 1 * Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.arc(450, 120, 60, 0, 1 * Math.PI);
+ctx.stroke();
+
+
